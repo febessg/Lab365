547203b38e713121539f6ffa070862af56dff6cf
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-04-11 14:52:54 -0300

    Update index.js

--- M1S08/OMRSequelize/index.js
+++ M1S08/OMRSequelize/index.js
@@ -11,12 +11,18 @@ const Cliente = require('./cliente');
     
 database.sync();
 
-
+//Criando dados na tabela
     await Cliente.create({
     name: 'Maria',
     cpf: '11122233344'
     })
 
+//Alterando dados da tabela
+    const clientes = await Cliente.findByPk(1);
+
+    clientes.name = 'Maria Rita';
+
+    await clientes.save();
 
 app.listen(3333, () => {
     console.log('Servidor online')
