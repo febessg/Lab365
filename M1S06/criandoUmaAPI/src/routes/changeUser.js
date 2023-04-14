762c5e64f3c8c37ab17c9ad50146968c9680927f
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-04-14 18:18:47 -0300

    Create changeUser.js

---
+++ M1S06/criandoUmaAPI/src/routes/changeUser.js
@@ -0,0 +1,28 @@
+const Router = require('express');
+
+const createUser = require("../controller/createUser");
+const deleteUser = require("../controller/deleteUser");
+
+const routes = new Router();
+
+/* router.post('/addUser', (req, res) => {
+    const object = req.body;
+    res.json(object);
+})
+
+router.delete('/deleteUser/:id', (req, res) => {
+    let deletedUser = users.filter((user) => {
+        return user.id == req.params.id;
+    })
+
+    if (deletedUser) { 
+        res.status(200).send("Usuário deletado.")
+    } else {
+        res.status(404).send('Usuário não encontrado.')
+    }
+}) */
+
+routes.post('/addUser', createUser);
+routes.delete('/deleteUser/:id', deleteUser);
+
+module.exports = routes;
