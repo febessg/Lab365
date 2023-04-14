84ae68c4d467c4c714100175ac93ba60a7e8190e
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-04-14 18:15:21 -0300

    Update index.js

--- M1S06/criandoUmaAPI/index.js
+++ M1S06/criandoUmaAPI/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
+const routes = require('./sources/routes/changeUser')
+
 const app = express();
 
 app.use(express.json());
 
-const usersRoutes = require('./routes/users')
-
-app.use('/users', usersRoutes)
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Servidor online!')
